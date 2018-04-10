9b4463df2e23895447332a7e6113a6c77d1c5fe7
Max Narvaez <narvae1@stolaf.edu>
2018-04-09 21:25:44 -0500

    Moved default ColorContainers to Color_Container.cpp

--- Color_Container.cpp
+++ Color_Container.cpp
@@ -4,6 +4,43 @@
 using namespace std;
 
 
+// The following default colors are based on the colors recognized by default in HTML
+// May need to add #ifndef #define statements to fix multiple definition error
+
+const ColorContainer AliceBlue		{ 0xF0F8FF };	// 0xF0F8FF
+const ColorContainer AntiqueWhite	{ 0xFAEBD7 };	// 0xFAEBD7
+const ColorContainer Aqua			{ 0x00FFFF };	// 0x00FFFF
+const ColorContainer Aquamarine		{ 0x7FFFD4 };	// 0x7FFFD4
+const ColorContainer Azure			{ 0xF0FFFF };	// 0xF0FFFF
+const ColorContainer Beige			{ 0xF5F5DC };	// 0xF5F5DC
+const ColorContainer Bisque			{ 0xFFE4C4 };	// 0xFFE4C4
+const ColorContainer Black			{ 0x000000 };	// 0x000000
+ColorContainer BlanchedAlmond	{ 0xFFEBCD };	// 0xFFEBCD
+ColorContainer Blue				{ 0x0000FF };	// 0x0000FF
+ColorContainer BlueViolet		{ 0x8A2BE2 };	// 0x8A2BE2
+ColorContainer Brown			{ 0xA52A2A };	// 0xA52A2A
+ColorContainer BurlyWood		{ 0xDEB887 };	// 0xDEB887
+ColorContainer CadetBlue		{ 0x5F9EA0 };	// 0x5F9EA0
+ColorContainer Chartreuse		{ 0x7FFF00 };	// 0x7FFF00
+ColorContainer Chocolate		{ 0xD2691E };	// 0xD2691E
+ColorContainer Coral			{ 0xFF7F50 };	// 0xFF7F50
+ColorContainer CornflowerBlue	{ 0x6495ED };	// 0x6495ED
+ColorContainer Cornsilk			{ 0xFFF8DC };	// 0xFFF8DC
+ColorContainer Crimson			{ 0xDC143C };	// 0xDC143C
+ColorContainer Cyan				{ 0x00FFFF };	// 0x00FFFF
+ColorContainer DarkBlue			{ 0x00008B };	// 0x00008B
+ColorContainer DarkCyan			{ 0x008B8B };	// 0x008B8B
+ColorContainer DarkGoldenRod	{ 0xB8860B };	// 0xB8860B
+ColorContainer DarkGray			{ 0xA9A9A9 };	// 0xA9A9A9
+ColorContainer DarkGrey			{ 0xA9A9A9 };	// TODO: Maybe use a copy constructor // 0xA9A9A9
+ColorContainer DarkGreen		{ 0x006400 };	// 0x006400
+ColorContainer DarkKhaki		{ 0xBDB76B };	// 0xBDB76B
+ColorContainer DarkMagenta		{ 0x8B008B };	// 0x8B008B
+ColorContainer DarkOliveGreen	{ 0x556B2F };	// 0x556B2F
+
+
+
+
 /**	Default Constructor for the ColorContainer class
 	Initializes r, g, b to 0 */
 
@@ -13,7 +50,7 @@ ColorContainer::ColorContainer() { r = 0; g = 0; b = 0; }
 /**	Constructor for the ColorContainer class
 	@param hexIn The (usually) hexadecimal value that will be split into r, g and b values */
 
-ColorContainer::ColorContainer( int hexIn ) {
+ColorContainer::ColorContainer( long hexIn ) {
 
 	r = (hexIn & 0xFF0000) >> 16;
 	g = (hexIn & 0x00FF00) >> 8;

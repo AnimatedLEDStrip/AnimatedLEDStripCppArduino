a06323080f37960dcb1a0a0aa163007d3c76ec67
Max Narvaez <narvae1@stolaf.edu>
2018-04-21 19:23:55 -0500

    Reorganization of methods

--- AnimatedLEDStrip.h
+++ AnimatedLEDStrip.h
@@ -14,24 +14,24 @@ public:
 
 
 	void
-		chase( int, direction, ColorContainer ),	// Deprecated - use multiPixelRun( int, direction, ColorContainer )
-		chase( int, direction, int, int, int ),	// Deprecated - use multiPixelRun( int, direction, int, int, int )
 		ripple(),	// Runs a single ripple animation (similar to chase but in both directions from a starting point and subsequent LEDs are dimmer)
 		flicker(),	// Runs a single flicker animation (strip is at a color/brightness and random LEDs are set to a different color/brightness for a very short time before returning to the strip's color)
 		alternate(),	// Runs a single alternate animation (alternates between two colors at the specified rate (amount of time between change)) -- Based on code from FRC 3130
-		fadePixelRed( int pixel, int startIntensity, int endIntensity, bool revertAtCompletion = false ),	// Runs a fade red animation (fades red in a pixel from specified intensity to another specified intensity) -- Based on code from FRC 3130
-		fadePixelGreen( int pixel, int startIntensity, int endIntensity, bool revertAtCompletion = false ),	// Runs a fade green animation (fades green in a pixel from specified intensity to another specified intensity) -- Based on code from FRC 3130
-		fadePixelBlue( int pixel, int startIntensity, int endIntensity, bool revertAtCompletion = false ),	// Runs a fade blue animation (fades blue in a pixel from specified intensity to another specified intensity) -- Based on code from FRC 3130
-		fadePixelAll( int pixel, int startRedIntensity, int startGreenIntensity, int startBlueIntensity, int endRedIntensity, int endGreenIntensity, int endBlueIntensity, bool revertAtCompletion = false ),	// Runs a fade all animation (fades all pixels from specified intensities to other specified intensities) -- Based on code from FRC 3130
+		fadePixelRed( int pixel, int startIntensity, int endIntensity, bool revertAtCompletion = false ),	// Runs a fade red animation (fades red in a pixel from specified intensity to another specified intensity)
+		fadePixelGreen( int pixel, int startIntensity, int endIntensity, bool revertAtCompletion = false ),	// Runs a fade green animation (fades green in a pixel from specified intensity to another specified intensity)
+		fadePixelBlue( int pixel, int startIntensity, int endIntensity, bool revertAtCompletion = false ),	// Runs a fade blue animation (fades blue in a pixel from specified intensity to another specified intensity)
+		fadePixelAll( int pixel, int startRedIntensity, int startGreenIntensity, int startBlueIntensity, int endRedIntensity, int endGreenIntensity, int endBlueIntensity, bool revertAtCompletion = false ),	// Runs a fade all animation (fades all pixels from specified intensities to other specified intensities)
 		fadeStripRed( int startIntensity, int endIntensity, bool revertAtCompletion = false ),
 		fadeStripGreen( int startIntensity, int endIntensity, bool revertAtCompletion = false ),
 		fadeStripBlue( int startIntensity, int endIntensity, bool revertAtCompletion = false ),
 		fadeStripAll( int startRedIntensity, int startGreenIntensity, int startBlueIntensity, int endRedIntensity, int endGreenIntensity, int endBlueIntensity, bool revertAtCompletion = false ),
 		wave(),	// Runs a single wave animation (runs through different colors for each pixel) -- Based on code from FRC 3130
 		blind(),	// Runs a single blind animation (quickly alternates between full and off) -- Based on code from FRC 3130
-		pixelRun(),	// Runs a single pixel run animation (the strip is set to one color and then (in order) each pixel is set to a different color - similar to chase but with only one pixel)
+		pixelRun(),	// Runs a single pixel run animation (the strip is set to one color and then (in order) each pixel is set to a different color - similar to multiPixelRun() but with only one pixel)
 		multiPixelRun( int spacing, direction chaseDirection, ColorContainer colorValues ),	// Runs a single multi-pixel run animation (similar to pixelRun() but with multiple LEDs at a specified spacing)
-		multiPixelRun( int spacing, direction chaseDirection, int rIn, int gIn, int bIn );	// Runs a single multi-pixel run animation (similar to pixelRun() but with multiple LEDs at a specified spacing)
+		multiPixelRun( int spacing, direction chaseDirection, int rIn, int gIn, int bIn ),	// Runs a single multi-pixel run animation (similar to pixelRun() but with	multiple LEDs at a specified spacing)
+		chase( int, direction, ColorContainer ),	// Deprecated - use multiPixelRun( int, direction, ColorContainer )
+		chase( int, direction, int, int, int );	// Deprecated - use multiPixelRun( int, direction, int, int, int )
 
 	template<class paletteType> void smoothChase( const paletteType& palette, direction movementDirection, uint8_t brightness );
 

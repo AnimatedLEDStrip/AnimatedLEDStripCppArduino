215eadc3c8e486db5ac680a3505dbdf3ff1bccbb
Max Narvaez <narvae1@stolaf.edu@stolaf.edu>
2018-05-09 18:22:02 -0500

    Updated GUI
    Added getColorHex method to return corresponding hex value for color name. Also found that right-clicking the name of a widget in the sidebar and selecting "Go To Slot" brings up a dialog that allows you to select a signal and then brings you to a new slot method created in the mainwindow class.
    
    Also, to access widget methods from mainwindow.cpp, use ui->[name]->[method]().

--- LEDProj_GUI/mainwindow.cpp
+++ LEDProj_GUI/mainwindow.cpp
@@ -12,3 +12,14 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_comboBox_3_currentIndexChanged(const QString &arg1)
+{
+    ui->lineEdit_3->setText(getColorHex(arg1));
+}
+
+void MainWindow::on_comboBox_2_currentIndexChanged(const QString &arg1)
+{
+    ui->lineEdit_2->setText(getColorHex(arg1));
+}
